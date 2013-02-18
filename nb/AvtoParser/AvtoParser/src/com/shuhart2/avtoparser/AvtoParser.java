397ebfdea0e9e18de4617464bcdfad12980401f3
shuhart2 <shuhart2@gmail.com>
2013-02-19 02:02:02 +0700

    Добавляем объявление на авито, уперлись в капчу

--- nb/AvtoParser/AvtoParser/src/com/shuhart2/avtoparser/AvtoParser.java
+++ nb/AvtoParser/AvtoParser/src/com/shuhart2/avtoparser/AvtoParser.java
@@ -4,10 +4,14 @@
  */
 package com.shuhart2.avtoparser;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
 
 
 /**
@@ -15,12 +19,50 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
  * @author newx
  */
 public class AvtoParser {    
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
+        Logger logger = Logger.getLogger ("");
+        //logger.setLevel (Level.OFF);
+        String login = "pavel_f@66.ru";
+        String password = "ewHJdsU54G";
+        String phone = "+71234567890";
+        
         HtmlUnitDriver driver = new HtmlUnitDriver();
-        driver.get("http://ya.ru/");
-        WebElement element = driver.findElement(By.id("text"));
-        element.sendKeys("tits");
-        element.submit();
+        driver.setJavascriptEnabled(true);
+        //WebDriver driver =new FirefoxDriver();
+        driver.get("http://avito.ru/");
+        driver.findElements(By.cssSelector(".logon > a")).get(1).click();//Нажимаем вход
+        
+        driver.findElement(By.id("user_name")).sendKeys(login);
+        driver.findElement(By.id("pass")).sendKeys(password);
+        driver.findElement(By.id("quick_expire")).click();
+        driver.findElement(By.id("quick_expire")).submit();
+        
+        driver.findElement(By.cssSelector(".first>a")).click();
+        
+        driver.findElement(By.id("fld_phone")).sendKeys(phone);
+        driver.findElement(By.id("select-region")).click();
+        
+        driver.findElements(By.cssSelector("#location_select>.loc")).get(0).findElement(By.cssSelector("option[value='621590']")).click();
+        
+        driver.findElements(By.cssSelector("#location_select>.loc")).get(1).findElement(By.cssSelector("option[value='621630']")).click();        
+        driver.findElement(By.id("apply_region")).click();
+        
+        driver.findElement(By.cssSelector("#fld_category_id option[value='11']")).click();
+        
+        driver.findElement(By.cssSelector("#flt_param_7 option[value='27']")).click();
+        driver.findElement(By.id("fld_title")).sendKeys("Продам");
+        driver.findElement(By.id("fld_description")).sendKeys("Все продам!");
+        driver.findElement(By.id("fld_price")).sendKeys(Keys.BACK_SPACE, "10000");
+        driver.findElement(By.id("fld_images")).sendKeys("/tmp/cats.jpg");
+        Thread.sleep(9000);
+        driver.findElement(By.id("fld_images")).sendKeys("/tmp/cats2.jpg");
+        Thread.sleep(9000);
+        driver.findElement(By.id("vas_free")).click();
+        
+        driver.findElement(By.id("form_submit")).click();
+        
+        System.out.println(driver.findElement(By.id("captcha_image")).getAttribute("src"));
+        driver.quit();
     }
     
 }
